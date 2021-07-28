4c23c2a374d5aa6e84ca0811e663b244527934ab
zebedal <you@example.com>
2021-07-28 12:44:47 +0100

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+/* const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin; */
 
 
 module.exports = {
@@ -18,8 +18,8 @@ module.exports = {
         filename: 'index.html',
         template: path.resolve(__dirname + '/src/index.html'),
         inject: 'head'
-      }),
-      new BundleAnalyzerPlugin(),
+      })
+      /* new BundleAnalyzerPlugin(), */
     ],
     module: {
       rules: [
